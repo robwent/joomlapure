5d82adb08ca8fa1cc2a46a478a19979c91ee7104
Robert Went <i@robertwent.com>
2013-08-26 18:13:07 +0100

    updating plugin check

--- source/tpl_joomlapure/pure/fields/mobiledetect.php
+++ source/tpl_joomlapure/pure/fields/mobiledetect.php
@@ -15,10 +15,10 @@ class JFormFieldMobiledetect extends JFormField
 	protected function getInput() {
 		jimport( 'joomla.plugin.helper' );
 
-		if (!JPluginHelper::isEnabled('system', 'pure_mobiledetect')) {
+		if (!JPluginHelper::isEnabled('system', 'joomlapure')) {
 			echo '<div class="alert alert-error" style="float:left">
 			<button type="button" class="close" data-dismiss="alert">×</button>
-			<p>'.JText::_('TPL_JOOMLAPURE_MOBILEDISABLED').'<br/><a href="http://extensions.robertwent.com/pure-mobiledetect" target="_blank">'.JText::_('TPL_JOOMLAPURE_MOBILEDOWNLOAD').'</a><br/><a href="'.JURI::root(true).'/administrator/index.php?option=com_plugins&view=plugins?&filter_search=pure+mobiledetect" target="_blank">'.JText::_('TPL_JOOMLAPURE_MOBILEPUBLISH').'</a></p></div>';
+			<p>'.JText::_('TPL_JOOMLAPURE_MOBILEDISABLED').'<br/><a href="https://github.com/robwent/joomlapure" target="_blank">'.JText::_('TPL_JOOMLAPURE_MOBILEDOWNLOAD').'</a><br/><a href="'.JURI::root(true).'/administrator/index.php?option=com_plugins&view=plugins?&filter_search=joomlapure" target="_blank">'.JText::_('TPL_JOOMLAPURE_MOBILEPUBLISH').'</a></p></div>';
 		} else {
 			echo '<div class="alert alert-success">
 			<button type="button" class="close" data-dismiss="alert">×</button><p>'.JText::_('TPL_JOOMLAPURE_MOBILEENABLED').'
